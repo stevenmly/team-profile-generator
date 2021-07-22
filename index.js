7e3c7b32ff1354079766190f13fcad1802e90c3e
Steven Ly <ly.steven16@gmail.com>
2021-07-21 23:49:58 -0500

    Working on prompts for manager info

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const managerQuestions = [
+    {
+        type: "input",
+        name: "managerName",
+        message: "What is the Manager's name?" + "(Required)"
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "What is the Manager's employee ID?" + "(Required)"
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the Manager's email address?" + "(Required)"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the Manager's office number?" + "(Required)"
+    }
+];
