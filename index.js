7ff9e83145be0680211a7fc3a716624ab5384eb3
Steven Ly <ly.steven16@gmail.com>
2021-07-23 05:13:49 -0500

    styling and changes to index

--- index.js
+++ index.js
@@ -81,12 +81,13 @@ const internQuestions = [
 function addManager() {
     inquirer.prompt(managerQuestions)
     .then(function(data) {
-        const name = data.name
-        const id = data.id
-        const email = data.email
+        const name = data.managerName
+        const id = data.managerId
+        const email = data.managerEmail
         const officeNumber = data.officeNumber
         const manager = new Manager(name, id, email, officeNumber)
-        // teamArray.push(manager)
+        teamArray.push(manager)
+        console.log(teamArray)
         addTeamMembers();
     })
 }
@@ -120,12 +121,13 @@ function addTeamMembers() {
 function addEngineer() {
     inquirer.prompt(engineerQuestions)
     .then(function(data) {
-        const name = data.name
-        const id = data.id
-        const email = data.email
+        const name = data.engineerName
+        const id = data.engineerId
+        const email = data.engineerEmail
         const github = data.github
         const engineer = new Engineer(name, id, email, github)
-        // teamArray.push(engineer)
+        teamArray.push(engineer)
+        console.log(teamArray)
         addTeamMembers()
     })
 }
@@ -133,12 +135,13 @@ function addEngineer() {
 function addIntern() {
     inquirer.prompt(internQuestions)
     .then(function(data) {
-        const name = data.name
-        const id = data.id
-        const email = data.email
+        const name = data.internName
+        const id = data.internId
+        const email = data.internEmail
         const school = data.school
         const intern = new Intern(name, id, email, school)
-        // teamArray.push(intern)
+        teamArray.push(intern)
+        console.log(teamArray)
         addTeamMembers()
     })
 }
@@ -154,6 +157,7 @@ function completeTeam() {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>My Team</title>
+
             <link rel="stylesheet" href="./styles.css">
         </head>
         <body>
@@ -164,20 +168,20 @@ function completeTeam() {
             `
             htmlArray.push(htmlBeginning);
 
-            for (let i = 1; i < teamArray.length; i++) {
+            for (let i = 0; i < teamArray.length; i++) {
                 let object = `
                 <div class="member-card">
                     <div class="card-top">
                         <h2>${teamArray[i].name}</h2>
-                        <h2>${teamArray[i].title}</h2>
+                        <h2>${teamArray[i].getRole()}</h2>
                     </div>
                     <div class="card-bottom list-group">
                         <li class="list-group-item">Employee ID: ${teamArray[i].id}</li>
-                        <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a>></li>
+                        <li class="list-group-item">Email: <a href="mailto:${teamArray[i].email}">${teamArray[i].email}</a></li>
                 `
                 if (teamArray[i].officeNumber) {
                     object += `
-                    <li class="list-group-item">${teamArray[i].officeNumber}</li>
+                    <li class="list-group-item">Office Number: ${teamArray[i].officeNumber}</li>
                     `
                 }
                 if (teamArray[i].github) {
